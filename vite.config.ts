94831871cbdc68a815c0446e4357796a66d8bec6
fermofou <fermofou@gmail.com>
2025-06-04 00:16:51 -0600

    nombre repo

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,15 @@
-import path from "path";
-import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  base: "/svgCreator/", // <- MUST match your repo name
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  // Add base path for GitHub Pages
-  base: "/svgcreator/",
   build: {
     outDir: "dist",
     assetsDir: "assets",
